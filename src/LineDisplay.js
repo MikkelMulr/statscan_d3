1fe2f3a108e882b7adfe6a396e56589247452971
Suleman Mulla <sulemankasim.mulla@dcmail.ca>
2019-11-04 14:52:48 -0500

    added dataset to linechart

--- src/LineDisplay.js
+++ src/LineDisplay.js
@@ -4,7 +4,7 @@ export default class LineDisplay {
 	constructor(dataset) {
 		this.h = 700;
 		this.w = 1230;
-		this.tempData = [];
+		this.tempData = dataset;
 
 		this.lineFun = d3
 			.line()
@@ -16,110 +16,105 @@ export default class LineDisplay {
 		this.buildLineChart();
 	}
 	buildLineChart() {
-		fetch('./data.json')
-			.then(data => data.json())
-			.then(data => {
-				// console.log(data.data);
-				this.tempData = data.data;
-				console.log(this.tempData);
 
-				let svg = d3
-					.select('#lineSpace')
-					.attr('width', this.w)
-					.attr('height', this.h);
+		console.log(this.tempData);
 
-				let defs = svg.append('defs');
+		let svg = d3
+			.select('#lineSpace')
+			.attr('width', this.w)
+			.attr('height', this.h);
 
-				let gradient = defs
-					.append('linearGradient')
-					.attr('id', 'gradient')
-					.attr('x1', '0%')
-					.attr('y1', '100%')
-					.attr('x2', '0%')
-					.attr('y2', '0%');
+		let defs = svg.append('defs');
 
-				let stop1 = gradient
-					.append('stop')
-					.attr('offset', '0%')
-					.attr('stop-color', '#004aba');
+		let gradient = defs
+			.append('linearGradient')
+			.attr('id', 'gradient')
+			.attr('x1', '0%')
+			.attr('y1', '100%')
+			.attr('x2', '0%')
+			.attr('y2', '0%');
 
-				let stop2 = gradient
-					.append('stop')
-					.attr('offset', '35%')
-					.attr('stop-color', '#ddeef4');
+		let stop1 = gradient
+			.append('stop')
+			.attr('offset', '0%')
+			.attr('stop-color', '#004aba');
 
-				let stop3 = gradient
-					.append('stop')
-					.attr('offset', '100%')
-					.attr('stop-color', '#fe3500');
+		let stop2 = gradient
+			.append('stop')
+			.attr('offset', '35%')
+			.attr('stop-color', '#ddeef4');
 
-				let viz = svg
-					.append('path')
-					.attr('d', this.lineFun(this.tempData))
-					.attr('stroke', 'url(#gradient)')
-					.attr('stroke-width', 10)
-					.attr('fill', 'none')
-					// .attr("fill", "url(#gradient)")
-					.attr("stroke-linecap", "round")
-					.attr("stroke-linejoin", "round")
+		let stop3 = gradient
+			.append('stop')
+			.attr('offset', '100%')
+			.attr('stop-color', '#fe3500');
 
-				let points = svg.append("g")
-					.attr("id", "linePoints");
+		let viz = svg
+			.append('path')
+			.attr('d', this.lineFun(this.tempData))
+			.attr('stroke', 'url(#gradient)')
+			.attr('stroke-width', 10)
+			.attr('fill', 'none')
+			// .attr("fill", "url(#gradient)")
+			.attr("stroke-linecap", "round")
+			.attr("stroke-linejoin", "round")
 
-				let point = points.selectAll("circle")
-					.data(this.tempData)
-					.enter()
-					.append("circle")
-					.attr("cx", d => (d.year - 1969.5) * 30)
-					.attr("cy", d => this.h / 1.44 - d.temp * 50)
-					.attr("r", "6")
-					.attr("stroke", "#383838")
-					.attr("stroke-width", 2)
-					.attr("fill", "#fff")
+		let points = svg.append("g")
+			.attr("id", "linePoints");
 
-				// let zero = svg.append("polyline")
-				// 	.attr("points", `0,${this.h / 1.44} ${this.w},${this.h / 1.44}`)
-				// 	.attr("stroke", "#383838")
-				// 	.attr("stroke-width", "2")
-				// 	.attr("stroke-linecap", "round")
-				// 	.attr("stroke-dasharray", "5")
+		let point = points.selectAll("circle")
+			.data(this.tempData)
+			.enter()
+			.append("circle")
+			.attr("cx", d => (d.year - 1969.5) * 30)
+			.attr("cy", d => this.h / 1.44 - d.temp * 50)
+			.attr("r", "6")
+			.attr("stroke", "#383838")
+			.attr("stroke-width", 2)
+			.attr("fill", "#fff")
 
-				let tmepLabels = svg.append("g")
-					.attr("id", "tempLabels");
+		// let zero = svg.append("polyline")
+		// 	.attr("points", `0,${this.h / 1.44} ${this.w},${this.h / 1.44}`)
+		// 	.attr("stroke", "#383838")
+		// 	.attr("stroke-width", "2")
+		// 	.attr("stroke-linecap", "round")
+		// 	.attr("stroke-dasharray", "5")
 
-				let labels = tmepLabels.selectAll("text")
-					.data(this.tempData)
-					.enter()
-					.append("text")
-					.text(d => d.temp)
-					.attr("class", "temp")
-					.attr("x", d => ((d.year - 1970) * 30) + 10)
-					// .attr("x", 0)
-					.attr("y", d => this.h / 1.44 - d.temp * 60)
-					.attr("font-size", "12px")
-					.attr("font-family", "sans-serif")
-					.attr("fill", "#666")
-					.attr("text-anchor", "start")
-					.attr("dy", "0.35em")
+		let tmepLabels = svg.append("g")
+			.attr("id", "tempLabels");
 
-				// let yearLabels = svg.append("g")
-				// 	.attr("id", "yearLabels");
+		let labels = tmepLabels.selectAll("text")
+			.data(this.tempData)
+			.enter()
+			.append("text")
+			.text(d => d.temp)
+			.attr("class", "temp")
+			.attr("x", d => ((d.year - 1970) * 30) + 10)
+			// .attr("x", 0)
+			.attr("y", d => this.h / 1.44 - d.temp * 60)
+			.attr("font-size", "12px")
+			.attr("font-family", "sans-serif")
+			.attr("fill", "#666")
+			.attr("text-anchor", "start")
+			.attr("dy", "0.35em")
 
-				// let years = yearLabels.selectAll("text")
-				// 	.data(this.tempData)
-				// 	.enter()
-				// 	.append("text")
-				// 	.text(d => d.year)
-				// 	.attr("class", "year")
-				// 	.attr("x", d => (d.year - 1970) * 30)
-				// 	// .attr("x", 0)
-				// 	.attr("y", this.h / 1.44 + 10)
-				// 	.attr("font-size", "12px")
-				// 	.attr("font-family", "sans-serif")
-				// 	.attr("fill", "#666")
-				// 	.attr("text-anchor", "start")
-				// 	.attr("dy", "0.35em")
-				// 	.attr("font-weight", "bold")
-			});
+		// let yearLabels = svg.append("g")
+		// 	.attr("id", "yearLabels");
+
+		// let years = yearLabels.selectAll("text")
+		// 	.data(this.tempData)
+		// 	.enter()
+		// 	.append("text")
+		// 	.text(d => d.year)
+		// 	.attr("class", "year")
+		// 	.attr("x", d => (d.year - 1970) * 30)
+		// 	// .attr("x", 0)
+		// 	.attr("y", this.h / 1.44 + 10)
+		// 	.attr("font-size", "12px")
+		// 	.attr("font-family", "sans-serif")
+		// 	.attr("fill", "#666")
+		// 	.attr("text-anchor", "start")
+		// 	.attr("dy", "0.35em")
+		// 	.attr("font-weight", "bold")
 	}
 }

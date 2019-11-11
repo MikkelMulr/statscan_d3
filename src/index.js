6a86df2c4471e3dc44a9be1ab51b9fe41dbd393a
Suleman Mulla <sulemankasim.mulla@dcmail.ca>
2019-11-11 11:16:17 -0500

    commented index.js file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
+// importing classes for graph and the legend
 import BarDisplay from './BarDisplay';
 import LineDisplay from './LineDisplay';
 import Legend from './legend';
 
+// variables to store the height, width, and dataset for the graph to be drawn
 let dataset = [];
 let width = 1200;
 let height = 600;
 
+// fetching the data from data.json file and parsing it through data.json()
 fetch('./data.json')
 	.then(data => data.json())
 	.then(data => {
+		// assigning the data to dataset array
 		dataset = data.data;
+
+		// creating new instance of bar chart with the specified height, width and imported data
 		let myBars = new BarDisplay(height, width, dataset);
+
+		// creating new instance of line chart with the specified height, width and imported data
 		let myLine = new LineDisplay(height, width, dataset);
 	});
 
-
+// creating a new instance of legend
 let mylegend = new Legend();

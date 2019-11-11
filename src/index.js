8891dea69930d38001c6ec3507763130da9afa33
Mike Mueller <muellerm.dev@gmail.com>
2019-11-11 11:26:05 -0500

    added header comment to index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/**
+ * Authors: Suleman, Usman, Michael
+ * Description: Two charts (line and bar) recieving data from an external set of data,
+ * one display the precipitation and the other temperature over the span of 40 years.
+ * The data is specific to the canadian artctic tundra from the years 1970 - 2010.
+ */
+
+
 // importing classes for graph and the legend
 import BarDisplay from './BarDisplay';
 import LineDisplay from './LineDisplay';
